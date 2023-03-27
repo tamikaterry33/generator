ad7054479588e3543d59812336fe9137d3002995
tamikaterry33 <dsangsterr8986@gmail.com>
2023-03-26 21:30:46 -0400

    added fucntion keys

---
+++ script.js
@@ -0,0 +1,20 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+
+  
+function generatePassword () {}
+label for=characterAmountNumber 
+
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
